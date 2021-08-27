bb1bfaf1b727b41a7cf8557fbc2f763d419d79ec
szx <1057095423@qq.com>
2021-08-27 18:10:22 +0800

    尝试自动获取正文文本

--- controllers/full_time_mage.js
+++ controllers/full_time_mage.js
@@ -17,15 +17,15 @@ const utils = require('../utils/utils');
 // 烂柯棋缘 https://www.ddyueshu.com/16_16202/
 
 const Params = {
-    catalogUrl: `https://www.gebiqu.com/biquge_42368`,
-    startName: `第六百三十章`,
+    catalogUrl: `https://www.ddyueshu.com/18_18099/`,
+    startName: `第七百六十一章`,
     endName: ``,
     fileName: `师兄_620-630.txt`
 }
 
 exports.fetchCatalog = async (ctx,next) => {
     let urls = [];
-    let isHasEndFlag = Params.endName&&Params.endName.length>0;
+    let isHasStartFlag = Params.startName&&Params.startName.length>0;
     let initIndex = -1;
     let isLoading = false;
 
@@ -38,50 +38,50 @@ exports.fetchCatalog = async (ctx,next) => {
         isLoading=true;
         // 20210809 注意：当网站的域名发生更换时，会出现superagent: double callback bug.
         const result = await superagent.get(Params.catalogUrl).buffer(true).charset('binary');
-        let html = result&&result.text;
-        var buf = Buffer.from(html, 'binary'); //转换成buffer
+        let html = result && result.text;
+        let buf = Buffer.from(html, 'binary'); //转换成buffer
         let charset = (buf.toString().match(/<meta.+?charset=['"]?([^"']+)/i) || []).pop() || 'utf-8'; //编码格式
-        var decodeHtml = iconv.decode(buf, charset); 
-        let $ = await cheerio.load(decodeHtml, {decodeEntities: false}); //用cheerio解析页面数据
+        var decodeHtml = iconv.decode(buf, charset);
+        let $ = await cheerio.load(decodeHtml, { decodeEntities: false }); //用cheerio解析页面数据
         let catalogNodeName = this.findCatalogNodeName($); //目录结构的标签名称 例如:li,dd
-        
+
         $(`${catalogNodeName} a`).each((index, element) => {
             let $text = $(element).text();
             let $url = $(element).attr('href');
-            if (isHasEndFlag&&($text.includes(Params.endName))) {
+            if (isHasStartFlag && ($text.includes(Params.startName))) {
                 initIndex = index;
             }
-            if (((isHasEndFlag&&initIndex>-1)||!isHasEndFlag)&&/第.*章.*/.test($text)) {
-                console.log('===$text===',$text);
+            if (((isHasStartFlag && initIndex > -1) || !isHasStartFlag) && /第.*章.*/.test($text)) { //((isHasStartFlag && initIndex > -1) || !isHasStartFlag) && /第.*章.*/.test($text)
+                // console.log('===$text===', $text);
                 urls.unshift($url);
             }
-            if ($text.includes(Params.startName)) {
+            if (utils.isDataValid(Params.endName)&&$text.includes(Params.endName)) {
                 return false;
             }
         });
-        console.log('获取数组完成:',JSON.stringify(urls),'数组长度:',urls.length);
+        console.log('获取数组完成:', JSON.stringify(urls), '数组长度:', urls.length);
 
-        // if (!utils.isDataValid(urls) || urls.length < 1) {
-        //     ctx.body = {errInfo: '抓取章节失败！'};
-        // }
+        if (!utils.isDataValid(urls) || urls.length < 1) {
+            ctx.body = {errInfo: '抓取章节失败！'};
+        }
 
-        // for (let index = 0; index < urls.length; index++) {
-        //     try {
-        //         await utils.delay(250);
-        //         let resStr = await this.fetchChapter(`${urls[index]}`);
-        //         if (resStr&&resStr.length>0) {
-        //             await utils.writeFile(Params.fileName,resStr);
-        //         }else {
-        //             ctx.body = {
-        //                 errInfo2: `${url}抓取失败`,
-        //             }
-        //         }
-        //     } catch (error) {
-        //         ctx.body = {
-        //             errInfo3: error,
-        //         }
-        //     }
-        // }
+        for (let index = 0; index < urls.length; index++) {
+            try {
+                await utils.delay(250);
+                let resStr = await this.fetchChapter(`${urls[index]}`);
+                // if (resStr&&resStr.length>0) {
+                //     await utils.writeFile(Params.fileName,resStr);
+                // }else {
+                //     ctx.body = {
+                //         errInfo2: `${url}抓取失败`,
+                //     }
+                // }
+            } catch (error) {
+                ctx.body = {
+                    errInfo3: error,
+                }
+            }
+        }
         isLoading = false;
     } catch (error) {
         isLoading = false;
@@ -96,23 +96,49 @@ exports.fetchChapter = async (chapterUrl) => {
     return new Promise(async (resolve, reject) => {
         let url = `${Params.catalogUrl}${chapterUrl}`;
         let str = '';
-        superagent.get(`${url}`).buffer(true).charset('gbk').then((res) => {
+        superagent.get(`${url}`).buffer(true).charset('binary').then((res) => {
             let html = res&&res.text;
-            let $ = cheerio.load(html, {decodeEntities: false}); //用cheerio解析页面数据
-            let header = $('h1').text().trim();
-            header = header.replace(/（.*?）/,'');
-            header = header.replace(/【.*?】/,''); 
-            str += ('\r\n' + header + '\r\n');
-            $('div#content').contents().each((index, element) => {
-                let $text = $(element).text().trim();
-                if ($text.indexOf('PS：')!=-1||$text.indexOf('PS:')!=-1||$text.indexOf('booktxt')!=-1) {
-                    return false;
-                }
-                if ($text&&$text.length > 0) { //不需要空行
-                    str += (`    ` + $text + '\r\n');
-                }
+            let buf = Buffer.from(html, 'binary'); //转换成buffer
+            let charset = (buf.toString().match(/<meta.+?charset=['"]?([^"']+)/i) || []).pop() || 'utf-8'; //编码格式
+            var decodeHtml = iconv.decode(buf, charset);
+            let $ = cheerio.load(decodeHtml, {decodeEntities: false}); //用cheerio解析页面数据
+            // let header = $('h1').text().trim();
+            // header = header.replace(/（.*?）/,'');
+            // header = header.replace(/【.*?】/,''); 
+            // str += ('\r\n' + header + '\r\n');
+            
+            let length = $('br').length;
+            console.log('===length===',length);
+            let count = $('br').parents().length;
+            console.log('===count===',count);
+            $('br').parents().each((index, element) => {
+                // let el = $(element).prop("tagName");
+                // console.log('===el===',el);
+                // let el = $(element).attr
+                // console.log('===el===',el);
+                // let name = element.name;
+                // let type = element.type;
+                // let attributes = element.attributes;
+                // let node_type = element.nodeType;
+                // let akb = element.attribs;
+                // console.log('===name===',name);
+                // console.log('===type===',type);
+                // console.log('===attributes===',attributes);
+                // console.log('===node_type===',node_type);
+                // console.log('===akb===',akb);
+                let attr = $(element).attr();
+                console.log('===attr===',attr);
             });
-            console.log('===抓取结束===',header);
+            // $('div#content').contents().each((index, element) => {
+            //     let $text = $(element).text().trim();
+            //     if ($text.indexOf('PS：')!=-1||$text.indexOf('PS:')!=-1||$text.indexOf('booktxt')!=-1) {
+            //         return false;
+            //     }
+            //     if ($text&&$text.length > 0) { //不需要空行
+            //         str += (`    ` + $text + '\r\n');
+            //     }
+            // });
+            // console.log('===抓取结束===',header);
             resolve&&resolve(str);
         }).catch((err)=>{
             reject&&reject(err)
@@ -133,14 +159,6 @@ exports.findCatalogNodeName = ($) => {
     return nodeName;
 }
 
-/** 获取网站的字符集 */
-exports.getWebsiteCharset = ($) => {
-    let charsetOfMeta = $('meta[charset]').attr('charset');
-    if (utils.isDataValid(charsetOfMeta)) return charsetOfMeta;
-    let charsetOfHE = $('meta[http-equiv]').attr('content');
-    if (utils.isDataValid(charsetOfHE)) {
-        let charset = charsetOfHE.split("charset=")[1];
-        if (utils.isDataValid(charset)) return charset;
-    }
-    return 'utf-8';
-}
+// exports.findContentNodeId = ($) => {
+//     $('br').
+// }
